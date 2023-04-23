a408593c9bff6b33428b1e8654a7f122772493f8
tronbodya <tronbodya@gmail.com>
2023-04-23 17:50:41 +0300

    fundamentals exercise

--- LearningMaterial/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
+++ LearningMaterial/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
@@ -10,8 +10,10 @@ public class CalculatorTests
     [InlineData(5, 4, 9)]
     [InlineData(0, 0, 0)]
     [InlineData(-5, -5, -10)]
-    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(
-        int a, int b, int expected)
+    [InlineData(5, 5, 10)]
+    [InlineData(-5, 5, 0)]
+    [InlineData(-15, -5, -20)]
+    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
     {
         // Act
         var result = _sut.Add(a, b);
@@ -20,13 +22,41 @@ public class CalculatorTests
         Assert.Equal(expected, result);
     }
 
-    [Fact]
-    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers()
+    [Theory]
+    [InlineData(5, 5, 0)]
+    [InlineData(-15, -5, -10)]
+    [InlineData(15, 5, 10)]
+    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
     {
         // Act
-        var result = _sut.Subtract(10, 5);
+        var result = _sut.Subtract(a, b);
 
         // Assert
-        Assert.Equal(5, result);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 25)]
+    [InlineData(5, -5, -25)]
+    [InlineData(50, 0, 0)]
+    public void Multiply_ShouldMultiplyTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
+    {
+        // Act
+        var result = _sut.Multiply(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 1)]
+    [InlineData(15, 5, 3)]
+    public void Divide_ShouldDivideTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
+    {
+        // Act
+        var result = _sut.Divide(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
     }
 }
